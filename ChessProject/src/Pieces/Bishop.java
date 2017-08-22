475e22479d75e75faf697b9d5c110a4972d4d95c
Marco <Marco@MarcoBOSS>
2017-08-22 20:59:43 +1200

    updated

--- ChessProject/src/Pieces/Bishop.java
+++ ChessProject/src/Pieces/Bishop.java
@@ -19,7 +19,7 @@ public class Bishop extends Piece{
 
     @Override
     public boolean isValidPath(int finalX, int finalY) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
